30d26b8d4c41baa9a71d9d00eb70a3c8e607ae82
Guill Robin <robin.guill@insa-rennes.fr>
2021-01-12 17:00:19 +0100

    Test negative association test

--- org.xtext.example.mydsl.parent/org.xtext.example.mydsl.ui/src/org/xtext/example/mydsl/ui/quickfix/UmlQuickfixProvider.java
+++ org.xtext.example.mydsl.parent/org.xtext.example.mydsl.ui/src/org/xtext/example/mydsl/ui/quickfix/UmlQuickfixProvider.java
@@ -3,7 +3,16 @@
  */
 package org.xtext.example.mydsl.ui.quickfix;
 
+import javax.swing.text.BadLocationException;
+
+import org.eclipse.xtext.ui.editor.model.IXtextDocument;
+import org.eclipse.xtext.ui.editor.model.edit.IModification;
+import org.eclipse.xtext.ui.editor.model.edit.IModificationContext;
 import org.eclipse.xtext.ui.editor.quickfix.DefaultQuickfixProvider;
+import org.eclipse.xtext.ui.editor.quickfix.Fix;
+import org.eclipse.xtext.ui.editor.quickfix.IssueResolutionAcceptor;
+import org.eclipse.xtext.validation.Issue;
+import org.xtext.example.mydsl.validation.UmlValidator;
 
 /**
  * Custom quickfixes.
@@ -12,15 +21,22 @@ import org.eclipse.xtext.ui.editor.quickfix.DefaultQuickfixProvider;
  */
 public class UmlQuickfixProvider extends DefaultQuickfixProvider {
 
-//	@Fix(UmlValidator.INVALID_NAME)
-//	public void capitalizeName(final Issue issue, IssueResolutionAcceptor acceptor) {
-//		acceptor.accept(issue, "Capitalize name", "Capitalize the name.", "upcase.png", new IModification() {
-//			public void apply(IModificationContext context) throws BadLocationException {
-//				IXtextDocument xtextDocument = context.getXtextDocument();
-//				String firstLetter = xtextDocument.get(issue.getOffset(), 1);
-//				xtextDocument.replace(issue.getOffset(), 1, firstLetter.toUpperCase());
-//			}
-//		});
-//	}
+	@Fix(UmlValidator.INVALID_NAME)
+	public void capitalizeName(final Issue issue, IssueResolutionAcceptor acceptor) {
+		acceptor.accept(issue, "Capitalize name", "Capitalize the name.", "upcase.png", new IModification() {
+			public void apply(IModificationContext context) throws BadLocationException {
+				IXtextDocument xtextDocument = context.getXtextDocument();
+				String firstLetter;
+				try {
+					firstLetter = xtextDocument.get(issue.getOffset(), 1);
+					xtextDocument.replace(issue.getOffset(), 1, firstLetter.toUpperCase());
+				} catch (org.eclipse.jface.text.BadLocationException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
 
 }
